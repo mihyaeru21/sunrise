9be0c84bac1990c56f2eb7a72e92f6bbd97b4df3
Mihyaeru <mihyaeru.app@gmail.com>
2013-02-25 16:21:47 +0900

    いらなそうな部分を削除

--- sunrise.py
+++ sunrise.py
@@ -1,34 +1,39 @@
 # -*- coding: utf-8 -*-
 
-from math import sin, cos, tan, asin, acos, atan
-from math import radians, degrees
+import math
 from math import floor, ceil
 import datetime
 
+# 外部から利用する関数
+def sunrise(lon, lat, date):
+    """日の出時刻"""
+    return _sunriseset(lon, lat, date, -1)
+
+def sunset(lon, lat, date):
+    """日の入り時刻"""
+    return _sunriseset(lon, lat, date, 1)
+
+
 # 度を使った三角関数
 def _sind(deg):
-    return sin(radians(deg))
+    return math.sin(math.radians(deg))
 def _cosd(deg):
-    return cos(radians(deg))
+    return math.cos(math.radians(deg))
 def _tand(deg):
-    return tan(radians(deg))
+    return math.tan(math.radians(deg))
 def _asind(value):
-    return degrees(asin(value))
+    return math.degrees(math.asin(value))
 def _acosd(value):
-    return degrees(acos(value))
+    return math.degrees(math.acos(value))
 def _atand(value):
-    return degrees(atan(value))
+    return math.degrees(math.atan(value))
 
-def _optimize_degrees(d):
-    deg = d
-    while deg < 0 or deg > 360:
-        if deg < 0: deg += 360
-        elif deg > 360: deg -= 360
+def _optimize_degrees(deg):
+    """ 角度を0-360にする """
+    deg = deg % 360
+    if deg < 0:
+        deg += 360
     return deg
-
-def _timevar(hour, minute, second):
-    """ 時間の単位を[日]に変換する """
-    return hour / 24.0 + minute / 1440.0 + second / 86400.0
 
 def _timevar_T(date, timevar):
     """ 時刻変数 T """
@@ -64,7 +69,7 @@ def _sun_ecliptic_longitude(t):
             + 0.0004 * _sind( 21.1  +  720.02 * t) \
             + 0.0003 * _sind(352.5  + 1079.97 * t) \
             + 0.0003 * _sind(329.7  +   44.43 * t)
-    return l
+    return _optimize_degrees(l)
 
 def _ecliptic_longitude_inclination(t):
     """ 黄経傾角 ε """
@@ -83,63 +88,64 @@ def _geocentric_distance(t):
 
 def _sun_right_ascension(t):
     """ 太陽の赤経 """
-    oukei = _sun_ecliptic_longitude(t)
-    oukei = _optimize_degrees(oukei)
-    keikaku = _ecliptic_longitude_inclination(t)
-    sekikei = _atand(_tand(oukei) * _cosd(keikaku))
-    sekikei = _optimize_degrees(sekikei)
+    sun_el = _sun_ecliptic_longitude(t)
+    inclination = _ecliptic_longitude_inclination(t)
+    ascension = _optimize_degrees(_atand(_tand(sun_el) * _cosd(inclination)))
     # 象限を補正 黄経λ, 赤経α
     # 0 <= λ < 180 ならば 0 <= α < 180
     # 180 <= λ < 360 ならば 180 <= α < 360
-    if (oukei >=0 and oukei < 180):
-       if sekikei >= 180: sekikei -= 180
-       if sekikei < 0: sekikei += 180
-    if (oukei >= 180 and oukei < 360):
-        if sekikei <= 180: sekikei += 180
-        if sekikei > 360: sekikei -= 180
-    return sekikei
+    if (sun_el >=0 and sun_el < 180):
+       if ascension >= 180: ascension -= 180
+       if ascension < 0: ascension += 180
+    if (sun_el >= 180 and sun_el < 360):
+        if ascension <= 180: ascension += 180
+        if ascension > 360: ascension -= 180
+    return ascension
 
 def _sun_declination(t):
     """ 太陽の赤緯 """
-    #return 0.00265 * d * d - 0.266037 * d - 17.94810859
     sun_el = _sun_ecliptic_longitude(t)
-    keikaku = _ecliptic_longitude_inclination(t)
-    return _asind(_sind(sun_el) * _sind(keikaku))
+    inclination = _ecliptic_longitude_inclination(t)
+    return _asind(_sind(sun_el) * _sind(inclination))
 
-def _sidereal_time(t, d, keido):
+def _sidereal_time(t, d, lon):
     """ 恒星時 """
-    #return 57.027999 + 360.9856474 * d
-    kj = 325.4606 + 360.007700536 * t + 0.00000003879 * t * t + 360.0 * d + keido
+    kj = 325.4606 + 360.007700536 * t + 0.00000003879 * t * t + 360.0 * d + lon
     return _optimize_degrees(kj)
 
-def _appearheight(kyori):
+def _appearheight(distance):
     """ 出没高度 """
-    s = 0.266994 / kyori
+    s = 0.266994 / distance
     e = 0
     r = 0.585556
-    pi = 0.0024428 / kyori
+    pi = 0.0024428 / distance
     k = - s - e - r + pi
     return k
 
-def _appearheight_to_hourangle(koudo, sekii, ido):
+def _appearheight_to_hourangle(height, declination, lat):
     """ 出没高度 -> 時角 """
-    t_k_p = (_sind(koudo) - _sind(sekii) * _sind(ido)) / (_cosd(sekii) * _cosd(ido))
+    t_k_p = (_sind(height) - _sind(declination) * _sind(lat)) / (_cosd(declination) * _cosd(lat))
     t_k = _acosd(t_k_p)
     return t_k
 
-def _sun_hourangle(kouseiji, sekikei):
+def _sun_hourangle(sidereal_time, ascension):
     """太陽時角"""
-    return kouseiji - sekikei
+    return sidereal_time - ascension
 
-def _revise(jikaku_k, jikaku_t):
-    dd = (jikaku_k - jikaku_t) / 360
+def _revise(ha_k, ha_t):
+    """ 補正値の算出 """
+    dd = (ha_k - ha_t) / 360
     # 整数部を削除
-    if dd >= 1: dd -= floor(dd)
-    if dd <= -1: dd -= ceil(dd)
+    if dd >= 1:
+        dd -= floor(dd)
+    if dd <= -1:
+        dd -= ceil(dd)
     # 絶対値が小さくなる方の補正値を選ぶ
     dd_p = 0
-    if dd > 0: dd_p = - (1.0 - dd)
-    else: dd_p = 1.0 + dd
+    if dd > 0:
+        dd_p = - (1.0 - dd)
+    else:
+        dd_p = 1.0 + dd
 
     if abs(dd) < abs(dd_p):
         return dd
@@ -159,42 +165,32 @@ def _timevar_to_time(d):
         minute = 0
     return datetime.time(hour=int(hour), minute=int(minute))
 
-def _sunrise_sunset(keido, ido, date, hourangle_func):
-    h = 12  # 探索の初期値
-    d = _timevar(h, 0, 0)
+def _sunriseset(lon, lat, date, ha_revise):
+    h = 12        # 探索の初期値
+    d = h / 24.0  # 時間の単位を[日]にする
     dd = 1.0
     while abs(dd) > 0.00005:
         t = _timevar_T(date, d)
         sekikei = _sun_right_ascension(t)
         sekii = _sun_declination(t)
-        kyori = _geocentric_distance(t)
-        kouseiji = _sidereal_time(t, d, keido)
-        k = _appearheight(kyori)
-        jikaku_k = hourangle_func(k, sekii, ido)
-        jikaku_t = _sun_hourangle(kouseiji, sekikei)
-        dd = _revise(jikaku_k, jikaku_t)
+        distance = _geocentric_distance(t)
+        kouseiji = _sidereal_time(t, d, lon)
+        k = _appearheight(distance)
+        ha_k = _appearheight_to_hourangle(k, sekii, lat) * ha_revise
+        ha_t = _sun_hourangle(kouseiji, sekikei)
+        dd = _revise(ha_k, ha_t)
         d += dd
     return d
 
-def sunrise(keido, ido, date):
-    """日の出時刻"""
-    hourangle_func = lambda koudo,sekii,ido: - (_appearheight_to_hourangle(koudo, sekii, ido))
-    return _sunrise_sunset(keido, ido, date, hourangle_func)
-
-def sunset(keido, ido, date):
-    """日の入り時刻"""
-    hourangle_func = lambda koudo,sekii,ido: _appearheight_to_hourangle(koudo, sekii, ido)
-    return _sunrise_sunset(keido, ido, date, hourangle_func)
-
-
 if __name__ == "__main__":
-    """テストメソッド"""
-    keido = 139.7447
-    ido = 35.6544
+    """テスト"""
+    # 東京
+    lon = 139.7447
+    lat = 35.6544
     date = datetime.datetime.now()
     for i in range(366):
-        dr = sunrise(keido, ido, date)
-        ds = sunset(keido, ido, date)
+        dr = sunrise(lon, lat, date)
+        ds = sunset(lon, lat, date)
         sunrise_time = _timevar_to_time(dr)
         sunset_time = _timevar_to_time(ds)
         print date, "%02d:%02d" % (sunrise_time.hour, sunrise_time.minute), "%02d:%02d" % (sunset_time.hour, sunset_time.minute)
